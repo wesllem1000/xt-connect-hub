6aa5f6729293a20d92d454f57dc24e61ec1c369d
wesllem <wesllem1000@gmail.com>
2026-04-20 11:19:16 -0300

    chore: iniciar branch xtconect-v2 limpa

--- supabase/migrations/20251203214627_2e1527e3-99ed-4e62-815e-57d20244e85e.sql
+++
@@ -1,9 +0,0 @@
--- Remove the overly permissive policy that exposes MQTT credentials to all users
-DROP POLICY IF EXISTS "Anyone can view active MQTT servers" ON public.mqtt_servers;
-
--- Create a more restrictive policy that only allows admins to view MQTT servers
--- Regular users will get credentials through the secure edge function
-CREATE POLICY "Only admins can view MQTT servers" 
-ON public.mqtt_servers 
-FOR SELECT 
-USING (has_role(auth.uid(), 'admin'::app_role));
